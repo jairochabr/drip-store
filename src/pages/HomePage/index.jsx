0dbb131fc5994970b1e78d7f0a983b230e535336
Alex Bessa <Alex Bessa>
2024-11-12 14:48:25 -0300

    feat: criando estrutura do imput

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,12 +2,19 @@ import { Loading } from "@/components/Loading";
 import { Carousel } from "./Carousel";
 import { FeaturedSection } from "./Featured";
 import { OfferSection } from "./Offer";
+import Input from "@/components/@ui/Input";
 
 export function HomePage() {
   return (
     <>
       <Loading />
       <Carousel />
+      <Input
+      htmlFor="input"
+      labelValue="Nome completo *"
+      id="input"
+      placeholder="Insira seu nome"
+      />
       <FeaturedSection />
       <OfferSection />
     </>
