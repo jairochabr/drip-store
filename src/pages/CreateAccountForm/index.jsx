07e1f17ee179532ea46f226300db59ba0f8df338
Alex Bessa <Alex Bessa>
2024-11-13 05:49:00 -0300

    evoluindo form

--- src/pages/CreateAccountForm/index.jsx
+++ src/pages/CreateAccountForm/index.jsx
@@ -7,17 +7,17 @@ export default function CreateAccountForm() {
         <>
            <S.Container>
                 <S.Titulo>Criar Conta</S.Titulo>
-                <BlockForm value="Informações pessoais">
-                    <Input
-                        htmlFor="nome"
-                        id="nome"
-                        labelValue="Nome Completo *"
-                        placeholder="Insira seu nome"
-                    />
-                </BlockForm>
-                <S.Newslatter>
-                    <input type="checkbox" />
-                    <p>Quero receber por email ofertas e novidades das lojas da Digital Store. A frequência de envios pode variar de acordo com a interação do cliente.</p>
+                    <BlockForm value="Informações pessoais">
+                        <Input
+                            htmlFor="nome"
+                            id="nome"
+                            labelValue="Nome Completo *"
+                            placeholder="Insira seu nome"
+                        />
+                    </BlockForm>
+                    <S.Newslatter>
+                        <input type="checkbox" />
+                        <p>Quero receber por email ofertas e novidades das lojas da Digital Store. A frequência de envios pode variar de acordo com a interação do cliente.</p>
                 </S.Newslatter>
             </S.Container> 
         </>
