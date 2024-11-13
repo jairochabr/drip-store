673bfe897b21a7a650445516ac0d0bb173b67a45
Jairo Rocha <jairochabr@gmail.com>
2024-11-13 18:58:24 -0300

    feat: Criação do component de icon ArrowRight

---
+++ src/components/icons/ArrowRight.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const ArrowRight = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" {...props}>
+    <path d="M15.5 7.5L20 12M20 12L15.5 16.5M20 12H4" stroke="#C92071" strokeWidth={1.8} strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+};
+
+export default ArrowRight;
