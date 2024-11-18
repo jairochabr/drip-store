272b1b06171518a3d9c67884ca67c02f45c077b2
Rhayssa Costa <laryssa97costa@gmail.com>
2024-11-18 20:59:48 -0300

    inserindo css com styled-components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,43 @@
 import { Link } from "react-router-dom";
 import Gmail from "../../assets/Login-fotos/gmail.png";
 import Facebook from "../../assets/Login-fotos/facebook.svg";
+import { LoginContainer } from "./style"
+import { BtnPrimary } from "../Buotoes/BtnPrimary/BtnPrimary";
 
-export const Login = () => {
+export function Login() {
     return (
-        <div>
-            <h1>Acesse sua conta</h1>
-            <p>Novo cliente? Então registre-se <Link>aqui</Link>. </p>
-            <label>Login</label>
-            <input type="text" placeholder="Insira seu login ou email"/>
-            <label>Senha</label>
-            <input type="text" placeholder="Insira sua senha"/>
-            <Link>Esqueci minha senha</Link>
-            <p>Acessar Conta</p>
+        <LoginContainer>
+            <h3 className='AcesseSuaConta'>Acesse sua conta</h3>
+
+            <form className="FormLogin">
+                <p className="NovoCliente">Novo cliente? Então registre-se <Link className="CliqueAqui" to='registro'>aqui</Link>. </p>
+                <div>
+                    <label className="LoginLabel">Login</label>
+                    <input type="text" placeholder="Insira seu login ou email" />
+                </div>
+                <div>
+                    <label className="Senha">Senha</label>
+                    <input type="password" placeholder="Insira sua senha" />
+                </div>
+                <Link>Esqueci minha senha</Link>
+                <BtnPrimary>Acessar Conta</BtnPrimary>
+            </form>
             <div>
-                <p>Ou faça login com </p>
-                <img src={Facebook}/> 
-                <img src={Gmail}/>
+                <div>
+                    <div>
+                        <p>Ou faça login com</p>
+                        <img src={Gmail} alt="Gmail" />
+                        <img scr={Facebook} alt="Facebook" />
+
+                    </div>
+
+
+
+
+                </div>
+
+
             </div>
-         </div>
-)
-};
+        </LoginContainer>
+    );
+}
---
+++ src/components/Login/style.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const LoginContainer = styled.div`
+    width: 58.3rem;
+    height: 56rem;
+    top: 20rem;
+    border-radius: .4rem;
+    padding: 3rem;
+    gap: 3rem;
+    background-color: white;
+;
+
+.AcesseSuaConta {
+    font-weight: 70rem;
+    font-size: 3.2rem;
+    line-height: 3.6rem;
+    letter-spacing: 0.1rem;
+    color: #1f1f1f;
+}
+
+.FormLogin {
+    font-weight: 52.3rem;
+    height: 2.8rem;
+    left: 3rem;
+    gap: 36.5rem;
+    top: 8.6rem;
+};
+
+.NovoCliente {
+    font-weight: 40rem;
+
+}`;
