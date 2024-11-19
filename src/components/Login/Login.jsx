0dab9a1048ac44be29a6b5cab0b1787097e97a10
rhayssacosta <rhayssa.costa@aluno.uece.br>
2024-11-19 20:10:26 -0300

    Finalizando a página login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,42 +2,39 @@ import { Link } from "react-router-dom";
 import Gmail from "../../assets/Login-fotos/gmail.png";
 import Facebook from "../../assets/Login-fotos/facebook.svg";
 import { LoginContainer } from "./style"
-import { BtnPrimary } from "../Buotoes/BtnPrimary/BtnPrimary";
 
 export function Login() {
     return (
         <LoginContainer>
             <h3 className='AcesseSuaConta'>Acesse sua conta</h3>
 
+            <p className="NovoCliente">Novo cliente? Então registre-se <Link className="CliqueAqui" to='registro'>aqui</Link>. </p>
+           
             <form className="FormLogin">
-                <p className="NovoCliente">Novo cliente? Então registre-se <Link className="CliqueAqui" to='registro'>aqui</Link>. </p>
-                <div>
-                    <label className="LoginLabel">Login</label>
-                    <input type="text" placeholder="Insira seu login ou email" />
-                </div>
-                <div>
-                    <label className="Senha">Senha</label>
-                    <input type="password" placeholder="Insira sua senha" />
-                </div>
-                <Link>Esqueci minha senha</Link>
-                <BtnPrimary>Acessar Conta</BtnPrimary>
+                
+                 <label className="LoginLabel">Login *</label>
+                 <input className= "InputLoginSenha"type="text" placeholder="Insira seu login ou email" />
+              
+                    <label className="LoginLabel">Senha *</label>
+                    <input className="InputLoginSenha" type="password" placeholder="Insira sua senha" />
+
+                
+                <Link className="LinkAqui">Esqueci minha senha</Link>
+                <button className="Button-Acesse">Acessar Conta</button>
             </form>
-            <div>
-                <div>
-                    <div>
+
+            {/* <div className="div-Login4"> */}
+            <p className="realizar-login-p">Ou faça login com</p>
+                    {/* <div>
                         <p>Ou faça login com</p>
                         <img src={Gmail} alt="Gmail" />
-                        <img scr={Facebook} alt="Facebook" />
+                        <img src={Facebook} alt="logo do facebook"/>
 
-                    </div>
-
-
+                    </div> */}
 
 
-                </div>
 
-
-            </div>
+            {/* </div> */}
         </LoginContainer>
     );
 }
--- src/components/Login/style.js
+++ src/components/Login/style.js
@@ -10,23 +10,102 @@ export const LoginContainer = styled.div`
     background-color: white;
 ;
 
-.AcesseSuaConta {
-    font-weight: 70rem;
-    font-size: 3.2rem;
-    line-height: 3.6rem;
-    letter-spacing: 0.1rem;
-    color: #1f1f1f;
+.login-container {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    width: 58.3rem;
+    height: 56rem;
+    top: 20rem;
+    left: 10rem;
+    border-radius: .4rem;
+    padding: 3rem;
+    gap: 3rem;
+    background-color: white;
 }
 
-.FormLogin {
-    font-weight: 52.3rem;
-    height: 2.8rem;
-    left: 3rem;
-    gap: 36.5rem;
-    top: 8.6rem;
+.AcesseSuaConta {
+    font-weight: 700;
+    font-size: 2rem;
+    line-height: 2.25rem;
+    letter-spacing: .0625rem;
+    color: var(--dark-gray);
 };
 
 .NovoCliente {
-    font-weight: 40rem;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.75rem;
+    align-items: center;
+    color: var(--dark-gray-2);
 
+};
+
+.CliqueAqui {
+    color: var(--dark-gray-2);
+
+};
+
+.FormLogin {
+    display: flex;
+    flex-direction: column;
+    font-size: .75rem;
+}; 
+.InputLoginSenha {
+    font-size: .75rem;
+    left: 3rem;
+    border-radius: .5rem;
+    border: none;
+    background-color: var(--light-gray-3);
+    padding: 1.25rem;
+    height: 2.1875rem;
+    width: 25rem;
+
+};
+
+.LinkAqui {
+    font-size: .625rem;
+    line-height: 1.375rem;
+    color: var(--light-gray-1);
+
+}
+
+.Senha {
+    display: flex;
+    flex-direction: column;
+    font-size: .75rem;
+};
+
+.EsqueciSenha {
+    font-weight: 500;
+    font-size: 1.4rem;
+    line-height: 2.2rem;
+    letter-spacing: .025rem;
+    text-decoration-skip-ink: none;
+};
+
+
+.Button-Acesse {
+    width: 25rem;
+    background-color: var(--primary);
+    height: 2.2rem;
+    border-radius: .5rem;
+    border: none;
+    color: var(--light-gray-3);
+    align-items: center;
+    letter-spacing: .0469rem;
+    
+
+
+};
+
+.div-Login4 {
+    margin: 0;
+    /* display: flex; */
+
+}
+
+.realizar-login-p {
+   /* margin-top: 20px; 
+   display: flex; */
 }`;
