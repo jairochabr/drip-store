dfd2dadf087b0b54cf6b23a5eb623eb2dbfb870b
Ingrid <stephanye.ingrid@gmail.com>
2024-11-21 10:12:23 -0300

    Feat/ Criação do componente OrdersInformations e montagem da OrderPage

--- src/pages/OrderPage/index.jsx
+++ src/pages/OrderPage/index.jsx
@@ -1,5 +1,37 @@
+import React, { useState } from "react";
+import { OrdersMenu } from "@/components/OrdersMenu";
+import { OrderStatus } from "@/components/OrderStatus";
+// import { UserProfile } from "@/components/UserProfile";
+import { UserInformations } from "@/components/OrderInformation";
+// import { PaymentMethods } from "@/components/PaymentMethods";
+import { OrderPageContainer, Sidebar, ContentArea } from "./style";
+
 export function OrderPage() {
+  const [selectedComponent, setSelectedComponent] = useState("profile");
+
+  const renderContent = () => {
+    switch (selectedComponent) {
+      // case "profile":
+      //   return <UserProfile />;
+      case "orders":
+        return <OrderStatus />;
+      case "informations":
+        return <UserInformations />;
+      // case "payment":
+      //   return <PaymentMethods />;
+      default:
+        return <OrderStatus />;
+    }
+  };
+
   return (
-    <h1>OrderPage</h1>
-  )
+    <OrderPageContainer>
+      <Sidebar>
+        <OrdersMenu onSelect={setSelectedComponent} />
+      </Sidebar>
+      <ContentArea>
+        {renderContent()}
+      </ContentArea>
+    </OrderPageContainer>
+  );
 }
